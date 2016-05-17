6b074d92e7e2c79aa433842b6c89a6af48fe5da1
baojizi@wacai.com <baojizi@wacai.com>
2016-05-17 10:03:48 +0800

    初始化工程

---
+++ LeSocket/LeSocket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeSocket
+//
+//  Created by caic on 16/5/17.
+//  Copyright © 2016年 caic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LeSocket/LeSocket/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LeSocket
+//
+//  Created by caic on 16/5/17.
+//  Copyright © 2016年 caic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
